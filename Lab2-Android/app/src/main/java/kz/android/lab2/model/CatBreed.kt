8c6bf08849e471e0c585b0a2f3f9e8047388358a
Aslan Mustafayev <116913574+aslanmyn@users.noreply.github.com>
2024-04-05 21:21:29 +0500

    Add files via upload

---
+++ Lab2-Android/app/src/main/java/kz/android/lab2/model/CatBreed.kt
@@ -0,0 +1,13 @@
+package kz.android.lab2.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CatBreed(
+    @SerializedName("name") val name: String,
+    @SerializedName("image_link") val imageLink: String,
+    @SerializedName("intelligence") val goodWithChildren: Int,
+    @SerializedName("grooming") val goodWithOtherDogs: Int,
+    @SerializedName("other_pets_friendly") val energy: Int,
+    @SerializedName("min_life_expectancy") val minLifeExpectancy: Int,
+    @SerializedName("max_life_expectancy") val maxLifeExpectancy: Int
+)
